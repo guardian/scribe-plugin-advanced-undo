b08936665bba36cd2477092db95522687e736312
Jon Parsons <jon.parsons@guardian.co.uk>
2015-02-20 13:08:34 +0000

    added limit method to diff creation

--- src/UndoController.js
+++ src/UndoController.js
@@ -1,27 +1,31 @@
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
 var virtualize = require('vdom-virtualize');
-var uuid = require('uuid');
+//Todo --> write our own throttle function as there is no need to import all of this
+//jp 20/2/15
+var _ = require('lodash');
 
-module.exports = class UndoController {
 
-  constructor(scribe, attrs){
-    //remove the default undo command as we will be replacing it
-    scribe.commands.undo.execute = function(){};
-    //keep a reference to this scribe instance as there can be multiple instances on the page
-    this.scribe = scribe;
-    //add an object to keep a list of our patches in reverse order
-    this.diffs = [];
-    //add a count to the number of undo commands
-    this.count = 0;
-    //set the last content so we have a point of comparison
-    this.lastContent = virtualize(this.scribe.el);
-    //add listeners
-    this.scribe.el.addEventListener('input', (e)=> this.onContentChanged(e));
-    this.scribe.el.addEventListener('keydown', (e)=> this.onKeyPressed(e));
-  }
+function UndoController(){
+  //remove the default undo command as we will be replacing it
+  scribe.commands.undo.execute = function(){};
+  //keep a reference to this scribe instance as there can be multiple instances on the page
+  this.scribe = scribe;
+  //add an object to keep a list of our patches in reverse order
+  this.diffs = [];
+  //add a count to the number of undo commands
+  this.count = 0;
+  //set the last content so we have a point of comparison
+  this.lastContent = virtualize(this.scribe.el);
+  //add listeners
+  this.scribe.el.addEventListener('input', (e)=> this.onContentChanged(e));
+  this.scribe.el.addEventListener('keydown', (e)=> this.onKeyPressed(e));
 
-  onKeyPressed(e){
+}
+
+UndoController.prototype = {
+  //undo / redo action
+  onKeyPressed: function(e){
     if(e.metaKey && e.keyCode === 90){
       e.preventDefault();
       //write the previous content into scribe's element
@@ -48,25 +52,29 @@ module.exports = class UndoController {
         range.selectNode(markers[0]);
         selection.addRange(range);
       }
-
       //increment the internal count
       this.count += 1;
     }
-  }
+  },
 
-  onContentChanged(){
+  onContentChanged: _.throttle(function(){
+    console.log('writing content');
     this._placeMarkers();
     var newContent = virtualize(this.scribe.el);
     var revertDiff = diff(newContent, this.lastContent);
     this.diffs.push(revertDiff);
     this.lastContent = newContent;
     this.count = 1;
-  }
+  }, 500),
 
-  //thin private wrapper for placing and removing markers before/after an action
-  _placeMarkers(fn){
+  //private util for placing markers
+  _placeMarkers: function(){
     var s = new this.scribe.api.Selection();
     s.placeMarkers();
   }
 
 };
+
+
+//export our class
+module.exports = UndoController;
