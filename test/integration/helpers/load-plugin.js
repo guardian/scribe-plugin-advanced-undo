8b3cdfadf7013695a195cb33f71ba213747d3375
Jon Parsons <jon.parsons@guardian.co.uk>
2015-02-24 12:36:52 +0000

    update loadplugin to exclude undo manager

--- test/integration/helpers/load-plugin.js
+++ test/integration/helpers/load-plugin.js
@@ -7,8 +7,11 @@ before(function(){
 
 module.exports = function loadPlugin() {
   return driver.executeAsyncScript(function (done) {
-    require(['../../../build/main'], function (plugin) {
-      window.scribe.use(plugin());
+    require(['scribe', '../../../build/main'], function (Scribe, plugin) {
+      var scribe = window.scribe = new Scribe(document.querySelector('.scribe'), {
+        undo: { enabled: true }
+      });
+      scribe.use(plugin());
       done();
     });
   });
